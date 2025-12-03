0f629fdaf79ed885db7d46dcaf94a211d10a7c27
imranmmd <mr.imranmammadov@gmail.com>
2025-12-03 15:38:58 +0400

    Modified schema

--- storage/db/schema.sql
+++ storage/db/schema.sql
@@ -24,3 +24,13 @@ CREATE TABLE IF NOT EXISTS chunks (
     token_count INTEGER,
     created_at TIMESTAMPTZ DEFAULT NOW()
 );
+CREATE TABLE IF NOT EXISTS chunk_embeddings (
+    chunk_id        VARCHAR PRIMARY KEY,
+    doc_id          VARCHAR,
+    order_index     INT,
+    page_number     INT,
+    header          TEXT,
+    token_count     INT,
+    text            TEXT,
+    embedding       vector(3072)        -- if using pgvector
+);
